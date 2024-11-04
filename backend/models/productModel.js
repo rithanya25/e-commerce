a9410f188e1fd529eea74ec762a18b42aa0d3dcf
rithanya25 <rithanyakee@gmail.com>
2024-11-04 15:00:12 +0530

    initial

---
+++ backend/models/productModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productSchema= new mongoose.Schema({
+    name: String,
+    price: String,
+    description: String,
+    ratings: String,
+    images: [
+        {
+            image: String
+        }
+    ],
+    category: String,
+    seller: String,
+    stock : String,
+    numOfReviewes: String,
+    createAt: Date
+})
+
+const productModel= mongoose.model('product',productSchema);
+module.exports =productModel;
